dc1faaaad524cd947022af77c628bd78dd9e16a8
Maksim Laurouski <maximlavrovsky@mail.ru>
2022-09-16 14:29:13 +0300

    Commit 6 finish run string in Remote

--- src/remote/Remote.tsx
+++ src/remote/Remote.tsx
@@ -4,15 +4,22 @@ import styleContainer from '../common/styles/Container.module.css';
 import Title from '../common/components/title/Title';
 
 const Remote = () => {
+    const contackMeText = []
+    for (let i=0; i<555; i++) {
+        contackMeText.push(` CONTACT ME -`.toUpperCase())
+    }
+    let textContact = contackMeText.map( t => {
+        return (
+            <span>{t}</span>
+        )
+    })
     return (
         <div className={style.RemoteBlock}>
             <div className={`${styleContainer.container} ${style.hireBlock}`}>
                 <Title title={'I am considering options for remote work'}/>
                 <div className={style.button}>
-                    <span> CONTACT ME -</span>
-                    <span> CONTACT ME -</span>
-                    <span> CONTACT ME -</span>
-                    <span> CONTACT ME -</span>
+                    {textContact}
+                    {textContact}
                      </div>
             </div>
         </div>
